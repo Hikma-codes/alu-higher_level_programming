f7317d57ce4717ac377691f709ed0d5ba17e14bf
Hikma-codes <h.hamza@alustudent>
2024-11-09 06:51:47 -0800

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""This is the module for MyList class."""
+"""Defines the inherited list class MyList."""
 
 
 class MyList(list):
-    """This os the custom list class."""
+    """This sorts the list class."""
 
     def print_sorted(self):
-        """This prints the list in ascending or minimum order."""
+        """This prints the list in ascending order."""
         print(sorted(self))
