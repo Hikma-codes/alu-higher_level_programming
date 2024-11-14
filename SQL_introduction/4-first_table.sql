b4a3708a47c6bb686ed648d6eff902196f1e952d
Hikma-codes <h.hamza@alustudent.com>
2024-11-14 08:02:58 -0800

    4-first_table.sql

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- This creates a table in the current database
+-- Query to create a table that's called first_table in the current database in your MySQL server
+CREATE TABLE IF NOT EXISTS first_table (
+id INT,
+name VARCHAR(256));
