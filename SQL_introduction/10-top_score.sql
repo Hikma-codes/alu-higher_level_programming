c2a2f1a14afc2b92fd3b3897da6e416dab5247d0
Hikma-codes <h.hamza@alustudent.com>
2024-11-14 08:14:04 -0800

    10-top_score.sql

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- This is a script that lists all records in a table
+-- Query to lists all the records in the table second_table of a database
+SELECT score, name FROM second_table ORDER BY score DESC;
