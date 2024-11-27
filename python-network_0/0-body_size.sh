e60bad5d80d3b64873e1b49deba57edd76ab9853
Hikma-codes <h.hamza@alustudent.com>
2024-11-27 16:55:37 +0000

    0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL,and sends a GET request to the URL, and displays the body of the response
+# Write a Bash script that takes in a URL, and sends a GET request to the URL, and displays the body of the response
 curl -sL "$1"
