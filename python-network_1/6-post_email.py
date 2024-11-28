50987927e4a25c09ab02ff6492a89cdf170d932d
Hikma-codes <h.hamza@alustudent.com>
2024-11-28 18:44:31 +0000

    6-post_email.py

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- Python script that takes in a URL and an email address,
+Python script that takes in a URL and an email address,
 sends a POST request to the passed URL with the email,
 and displays the body of the response
 """
@@ -11,4 +11,3 @@ import sys
 if __name__ == "__main__":
     r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(r.text)
-
