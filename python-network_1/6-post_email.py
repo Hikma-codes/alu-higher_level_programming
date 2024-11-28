535f299b1062f583ca0a0bd592b19815cfebedc2
Hikma-codes <h.hamza@alustudent.com>
2024-11-28 18:42:09 +0000

    6-post_email.py

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-The Python script that takes in a URL and an email address,
+ Python script that takes in a URL and an email address,
 sends a POST request to the passed URL with the email,
 and displays the body of the response
 """
