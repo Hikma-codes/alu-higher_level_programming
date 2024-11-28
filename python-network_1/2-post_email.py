28ef6641299b0ed30d753953921a8693180f9eea
Hikma-codes <h.hamza@alustudent.com>
2024-11-28 18:22:46 +0000

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -2,7 +2,7 @@
 """The script that:
 - takes in a URL
 - sends a POST request to the passed URL
-- takes email as  parameter
+- takes email as a parameter
 - displays the body of the response
 """
 import sys
