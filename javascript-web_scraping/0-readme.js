3b9f0867ddd3222473c9b0eaa99c3039f92e6707
Hikma-codes <h.hamza@alustudent.com>
2025-02-05 10:38:02 +0000

    0-readme.js

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+t readFile = () => {
+  const args = process.argv;
+  const arg = args[2];
+
+  const fs = require('fs');
+
+  fs.readFile(arg, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
+};
+readFile();
